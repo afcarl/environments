112a33bd61831f46d42ca3feaf9eb4740c4f0297
Fabien Benureau <fabien.benureau+git@gmail.com>
2015-10-29 22:34:14 +0100

    versioneer support

--- environments/__init__.py
+++ environments/__init__.py
@@ -9,3 +9,9 @@ from .prims import ConcatSPrimitive
 from .prims import PrimitiveEnvironment
 
 from . import tools
+
+from ._version import get_versions
+__version__ = get_versions()["version"]
+__commit__ = get_versions()["full-revisionid"]
+__dirty__ = get_versions()["dirty"]
+del get_versions
--- setup.py
+++ setup.py
@@ -1,23 +1,28 @@
 import os
 from setuptools import setup
 
+import versioneer
+
 setup(
-    name = "environments",
-    version = "1.0",
-    author = "Fabien Benureau",
-    author_email = "fabien.benureau@inria.fr",
-    url='flowers.inria.fr',
-    maintainer='Fabien Benureau',
-    description = "Blackbox environment interface and implementations for autonomous exploration of sensorimotor spaces",
-    license = "Open Science License (see fabien.benureau.com/openscience.html)",
-    keywords = "exploration algorithm blackbox",
-    download_url='https://github.com/humm/environments/tarball/1.0',
-    packages=['environments', 'environments.envs',
-             ],
+    name         = 'environments',
+    version      = '1.0',
+    cmdclass     = versioneer.get_cmdclass(),
+    author       = 'Fabien Benureau',
+    author_email = 'fabien.benureau@inria.fr',
+    url          = 'github.com/humm/environments.git',
+    maintainer   = 'Fabien Benureau',
+    description  = 'Blackbox environment interface and implementations for autonomous exploration of sensorimotor spaces',
+    license      = 'Open Science License (see fabien.benureau.com/openscience.html)',
+    keywords     = 'exploration algorithm blackbox',
+    download_url = 'https://github.com/humm/environments/tarball/1.0',
+    packages     = ['environments',
+                    'environments.envs',
+                    'environments.mprims',
+                   ],
+    requires     = ['forest', 'numpy'],
     dependency_links=[
         "https://github.com/flowersteam/forest/tarball/master#egg=forest-1.0",
     ],
-    requires=['forest', 'numpy'],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Programming Language :: Python :: 2.7',
