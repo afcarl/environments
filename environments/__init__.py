30c1cc66d7a9d976a9a0618effe491f03ae01b43
Fabien C. Y. Benureau <fabien.benureau+git@gmail.com>
2016-02-07 20:17:10 +0100

    __url__ attribute

--- environments/__init__.py
+++ environments/__init__.py
@@ -1,5 +1,17 @@
 from __future__ import absolute_import
 
+
+# versioneer
+from ._version import get_versions
+__version__ = get_versions()["version"]
+__commit__ = get_versions()["full-revisionid"]
+__dirty__ = get_versions()["dirty"]
+del get_versions
+
+__url__ = 'https://github.com/humm/environments'
+
+
+# intra-package imports
 from .environment import Channel
 from .environment import Environment
 
@@ -9,9 +21,3 @@ from .prims import ConcatSPrimitive
 from .prims import PrimitiveEnvironment
 
 from . import tools
-
-from ._version import get_versions
-__version__ = get_versions()["version"]
-__commit__ = get_versions()["full-revisionid"]
-__dirty__ = get_versions()["dirty"]
-del get_versions
